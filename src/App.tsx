acfe02de7b1043fd4da5eb84e5f7695d6e135e1d
John <demay.10@osu.edu>
2023-11-24 17:57:31 -0800

    Add more than two options

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { AppBar, Toolbar, Paper, Button, Container, Typography, Stack, TextField, Modal, Box } from '@mui/material';
-
-// import image from '../static/images/katAndJohn.jpg';
 
 const appStyle = {
   textAlign: 'center',
@@ -10,7 +8,6 @@ const appStyle = {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
-  // backgroundImage: `url(${image})`
   backgroundColor: '#ADD8E6',
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
@@ -43,22 +40,23 @@ const modalStyle = {
   boxShadow: 24
 }
 
+type Option = {
+  id: number,
+  value: string
+}
+
+const OPTION_LIMIT: number = 5;
+
 const App = () => {
-  const [option1, setOption1] = useState<string>("");
-  const [option2, setOption2] = useState<string>("");
+  const [options, setOptions] = useState<Option[]>([
+    {id: 1, value: ""}, {id: 2, value: ""}
+  ]);
   const [decided, setDecided] = useState("");
   const [modal, setModal] = useState(false);
 
   const decider = () => {
-    const max = 2;
-    const min = 1;
-    const decisionValue = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(decisionValue);
-    if (decisionValue === 2) {
-      setDecided(option2);
-    } else {
-      setDecided(option1);
-    }
+    const decisionValue: number = Math.floor(Math.random() * options.length);
+    setDecided(options[decisionValue].value);
     setModal(true);
   }
 
@@ -68,8 +66,22 @@ const App = () => {
   }
 
   const clearOptions = () => {
-    setOption1("");
-    setOption2("");
+    setOptions([{id: 1, value: ""}, {id: 2, value: ""}]);
+  }
+
+  const addAnotherOption = () => {
+    setOptions([...options, { id: options.length+1, value: ""}]);
+  }
+
+  const handleOptionsChange = (event: ChangeEvent<HTMLInputElement>, id: number): void => {
+    const existingOptions: Option[] = options;
+    const index: number = existingOptions.findIndex((option: Option) => id === option.id);
+    if (index >= 0) {
+      existingOptions[index].value = event.target.value;
+      setOptions([...existingOptions]);
+    } else {
+      throw Error(`Can't find item with id [${id}] in the options`);
+    }
   }
 
   return (
@@ -85,29 +97,35 @@ const App = () => {
         <Stack spacing={5} padding="2em 1em">
           <Typography variant='h4'>What's the decision?</Typography>
           <Stack direction="column" spacing={2}>
-            <TextField
-              variant='outlined'
-              size='small'
-              label="Option 1"
-              value={option1}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setOption1(e.target.value)}
-            />
-            <TextField
-            variant='outlined'
-            size='small'
-            label="Option 2"
-            value={option2}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setOption2(e.target.value)}
-            />
+            {options.map((option: Option) => {
+              return (
+                <TextField
+                   variant='outlined'
+                   key={`OptionInput${option.id}`}
+                   size='small'
+                   label={`Option ${option.id}`}
+                   value={option.value}
+                   onChange={(e: ChangeEvent<HTMLInputElement>) => handleOptionsChange(e, option.id)}
+                />
+              )
+            })}
           </Stack>
           <Container maxWidth="md">
             <Stack
               spacing={1}
             >
+              <Button
+                variant='contained'
+                color="primary"
+                onClick={() => addAnotherOption()}
+                disabled={OPTION_LIMIT === options.length}
+              >
+                I need more options!
+              </Button>
               <Button 
                 variant='contained'
                 sx={buttonStyle}
-                disabled={option1 === "" || option2 === ""}
+                disabled={options.length !== options.filter((option: Option) => option.value !== "").length}
                 onClick={() => decider()}
               >
                 Decide For Me!
@@ -118,7 +136,6 @@ const App = () => {
                 Clear
               </Button>
             </Stack>
-            
           </Container>
         </Stack>
       </Paper>
